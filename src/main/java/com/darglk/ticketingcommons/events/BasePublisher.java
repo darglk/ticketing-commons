06fdcda70b390ab45c91bd8fc346f13a46140841
darglk <darglk@gmail.com>
2022-05-04 17:27:20 +0200

    change base publisher to use kafka template

--- src/main/java/com/darglk/ticketingcommons/events/BasePublisher.java
+++ src/main/java/com/darglk/ticketingcommons/events/BasePublisher.java
@@ -2,25 +2,22 @@ package com.darglk.ticketingcommons.events;
 
 import com.darglk.ticketingcommons.events.types.Subjects;
 import com.darglk.ticketingcommons.utils.JSONUtils;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.nats.client.Connection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @AllArgsConstructor
 @Getter
 @Slf4j
 public abstract class BasePublisher {
     private Subjects subject;
-    private Connection connection;
+    private KafkaTemplate<String, String> connection;
 
     public void publish(Object data) {
-        ObjectMapper objectMapper = new ObjectMapper();
         String dataStr = JSONUtils.toJson(data);
 
         log.info("Publishing event {} to subject {}", dataStr, subject.getSubject());
-        this.connection.publish(subject.getSubject(), dataStr.getBytes());
+        this.connection.send(subject.getSubject(), dataStr);
     }
 }
